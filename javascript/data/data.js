da147abb6149f3fbe265705f073bbd48526f7148
eslam1998-dev <eslam1998-dev>
2021-09-05 12:21:18 +0200

    uploading all files

---
+++ javascript/data/data.js
@@ -0,0 +1,23 @@
+function myData() {
+    const XHR = new XMLHttpRequest();
+    XHR.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const obj = JSON.parse(this.responseText);
+            //services section
+            const servicesSec = obj["services-section"];
+            for (let i = 0; i < servicesSec.length; i++) {
+                let content = `
+                <div class='bord'>
+                    <h2>${servicesSec[i].h2}</h2>
+                    <p>${servicesSec[i].p}</p>
+                    <button>${servicesSec[i].button}</button>
+                </div>
+                `
+               $(".services.parent .content .bords").append(content)
+            }
+        }
+    }
+    XHR.open("GET","javascript/data/data.json");
+    XHR.send();
+}
+export default myData();
---
+++ javascript/main.js
@@ -0,0 +1,40 @@
+//switch between sections
+$(".point-container ul li").on("click", function () {
+    let sectionPostion = $("." + $(this).data("section")).offset().top;
+    $("html, body").animate({
+        scrollTop:sectionPostion
+    },"slow")
+})
+
+$(window).on("scroll", function () {
+    $(".parent-container .parent").each(function () {
+        if ($(window).scrollTop() > $(this).offset().top - 50) {
+            let item = $(this).attr("id");
+            let ele = $(".point-container li[data-section=" + item +"]")
+            ele.addClass("active").siblings().removeClass("active")
+            
+            if ($(this).hasClass("txt-black")) {
+                $(".body-container").removeClass("txt-white")
+            } else {
+                $(".body-container").addClass("txt-white")
+            }
+        }
+    })
+})
+
+//import data file
+import myData from "./data/data.js"
+
+//use slider effect
+$(".works.parent .slider").slick({
+    infinite:true,
+    appendArrows:$(".works.parent .slider"),
+    nextArrow:`<i class="slick-arrow slick-next fas fa-chevron-right"></i>`,
+    prevArrow:`<i class="slick-arrow slick-prev fas fa-chevron-left"></i>`
+})
+
+//menu-bar
+$(".menu-bar").on("click", function () {
+    $(this).toggleClass("active")
+    $(".menu-section").toggleClass("active")
+})
